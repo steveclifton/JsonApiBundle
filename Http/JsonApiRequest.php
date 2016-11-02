4f44fe003420078a8ca2fe248b2ea0bcdcf209d9
Ken Golovin <ken@realestate.co.nz>
2016-11-02 16:48:41 +1300

    Json Api Request helper

---
+++ Http/JsonApiRequest.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+ * This file is part of the Mango package.
+ *
+ * (c) Steffen Brem <steffenbrem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mango\Bundle\JsonApiBundle\Http;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * JsonApi Request
+ */
+class JsonApiRequest
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+    
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->request = $requestStack->getMasterRequest();
+    }
+
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return $this->request->query->get('filters', []);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getIncludedRelationships()
+    {
+        $includeParam = $this->request->query->get('include', []);
+        
+        $included = [];
+        
+        if (is_string($includeParam) && strlen($includeParam)) {
+            $included = array_map('trim', explode(',', $includeParam));
+            $included = array_filter($included);
+        }
+        
+        return $included;
+    }
+
+    /**
+     * @param integer $default
+     * @return integer
+     */
+    public function getPaginationLimit($default)
+    {
+        $pagination = $this->request->query->get('page', []);
+        
+        return isset($pagination['limit']) ? $pagination['limit'] : $default;
+    }
+    
+    /**
+     * @return integer
+     */
+    public function getPaginationOffset()
+    {
+        $pagination = $this->request->query->get('page', []);
+
+        return isset($pagination['offset']) ? $pagination['offset'] : 0;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSort()
+    {
+        $sortParam = $this->request->query->get('sort', '');
+
+        $sort = [];
+        
+        if (!empty($sortParam) && is_string($sortParam)) {
+            $fields = array_map('trim', explode(',', $sortParam));
+
+            foreach ($fields as $field) {
+                $key = ltrim($field, '-');
+                $sort[$key] = '-' === $field[0] ? 'DESC' : 'ASC';
+            }
+        }
+
+        return $sort;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFields()
+    {
+        $fieldParamss = $this->request->query->get('fields', []);
+        $fieldParamss = array_filter($fieldParamss);
+
+        $fields = [];
+        
+        foreach ($fieldParamss as $type => $members) {
+            $fields[$type] = [];
+            
+            $members = explode(',', $members);
+            $members = array_map('trim', $members);
+            
+            foreach ($members as $member) {
+                $fields[$type][] = $member;
+            }
+        }
+        
+        return $fields;
+    }
+}
